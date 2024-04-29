5c7161c123f5421343baa20063e001ba29920a37
Himanshu Jindal <77575482+himanshujindal20@users.noreply.github.com>
2024-04-29 06:38:30 +0530

    Update App.jsx

--- borderpayIo-master/src/App.jsx
+++ borderpayIo-master/src/App.jsx
@@ -2,15 +2,24 @@ import Contract from "./pages/Contract"
 import EmployeeList from "./pages/EmployeeList"
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
+import Home from "./pages/Home"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <div>
-      {/* <Signup /> */}
-      {/* <Login /> */}
-      <Contract />
-      <EmployeeList />
-    </div>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route path="/" exact><Home /></Route>
+            <Route path="/create-contract" exact> <Contract /></Route>
+            <Route path="/signup" exact> <Signup /></Route>
+            <Route path="/login" exact> <Login /></Route>
+            <Route path='/employee-list' exact><EmployeeList /></Route>
+          </Switch>
+        </Router>
+      </div>
   )
 }
 
